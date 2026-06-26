c8b2678d755f78baf313a86c8af6e20a283971ce
Martuluv <martuguada.n5@gmail.com>
2026-06-25 22:39:51 -0300

    agregue un mensaje cuando interactuan con contacto

--- js/contacto.js
+++ js/contacto.js
@@ -5,3 +5,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const carrito = obtenerCarrito();
   actualizarContador(carrito);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form-contacto");
+
+  if (!form) return;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    mostrarMensaje("Gracias por comunicarte con nosotros 💜");
+
+    form.reset();
+  });
+});
+
+const mostrarMensaje = (mensaje) => {
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.textContent = mensaje;
+
+  document.body.appendChild(toast);
+
+  setTimeout(() => {
+    toast.classList.add("show");
+  }, 100);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 2500);
+};
